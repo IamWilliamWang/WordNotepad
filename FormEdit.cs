990023f1bbd93aef017dee071f6c7d8537613cb2
William Wang <iamjerichoholic@hotmail.com>
2019-05-27 16:44:40 +0800

    自动聚焦改良

--- FormEdit.cs
+++ FormEdit.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace 日志书写器
@@ -443,7 +444,9 @@ namespace 日志书写器
 
         private void textBoxMain_MouseHover(object sender, EventArgs e)
         {
-            this.Activate();
+            int selectStart = this.textBoxMain.SelectionStart; // 保存位置
+            Mouse.DoMouseClick(); // 鼠标点击
+            this.textBoxMain.Select(selectStart, 0); // 还原位置
         }
         #endregion
 
@@ -505,5 +508,27 @@ namespace 日志书写器
             DarkModeSwitch();
         }
         #endregion
+
+        class Mouse
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+            public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+            private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+            private const int MOUSEEVENTF_LEFTUP = 0x04;
+            private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+            private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+
+            public static void DoMouseClick()
+            {
+                //Call the imported function with the cursor's current position
+                uint X = (uint)Cursor.Position.X;
+                uint Y = (uint)Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+            }
+
+            //...other code needed for the application
+        }
     }
 }
